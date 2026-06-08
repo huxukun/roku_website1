a639aa1f33895477d39e0d6b3bd14c7f041792a7
RokuHu <3375343167@qq.com>
2026-06-08 22:32:51 +0800

    fix: 降级Vite到5.x，简化配置，避免依赖问题

--- myWebSite/vite.config.js
+++ myWebSite/vite.config.js
@@ -1,26 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  css: {
-    devSourcemap: true,
-    modules: {
-      localsConvention: 'camelCase'
-    }
-  },
   publicDir: 'public',
   optimizeDeps: {
     include: ['three', 'three/examples/jsm/controls/OrbitControls']
-  },
-  build: {
-    cssMinify: false,
-    cssCodeSplit: false,
-    sourcemap: false,
-    target: 'es2015',
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
   }
 })
